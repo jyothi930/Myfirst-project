26d3e90cf36a535a633eabe37bd71041ec7d4d3e
jyoth930 <jyothimalla22@gmail.com>
2024-07-11 16:34:27 +0530

    first commit is done

---
+++ Addition.java
@@ -0,0 +1,11 @@
+Class Addition
+{
+public static void main(String args[]){
+int num1=40;
+int num2=50;
+int add=num1+num2;
+System.out.println("addition of two numbers"+add);
+}
+}
+
+ 
---
+++ Even.java
@@ -0,0 +1,13 @@
+Class Even
+{
+public static void main(String args[]){
+int num=20;
+if(num%10==0)
+{
+System.out.println(" given number is Even");
+}
+else{
+System.out.println("given number id Odd");
+}
+}
+}
